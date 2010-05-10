a2785db6e01a82c26881fd71b39166abfbc9bf16
Daniel Heath <daniel.r.heath@gmail.com>
2010-05-10 16:45:01 +1000

    Still fiddling

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,8 @@ if ENV["MONGOHQ_URL"]
   mongo_uri = URI.parse(ENV["MONGOHQ_URL"])
   ENV['MONGOID_HOST'] = mongo_uri.host
   ENV['MONGOID_PORT'] = mongo_uri.port.to_s
-  ENV["MONGOID_USERNAME"] = mongo_uri.user
+  ENV['MONGOID_USERNAME'] = mongo_uri.user
+  raise mongo_uri.user
   ENV['MONGOID_PASSWORD'] = mongo_uri.password
   ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub("/", "")
 end
