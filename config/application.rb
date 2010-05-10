399976178d42d32a0b4a9b4b3dc69c54384315ef
Daniel Heath <daniel.r.heath@gmail.com>
2010-05-10 16:46:49 +1000

    Still fiddling

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ if ENV["MONGOHQ_URL"]
   ENV['MONGOID_HOST'] = mongo_uri.host
   ENV['MONGOID_PORT'] = mongo_uri.port.to_s
   ENV['MONGOID_USERNAME'] = mongo_uri.user
-  raise mongo_uri.user
   ENV['MONGOID_PASSWORD'] = mongo_uri.password
   ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub("/", "")
 end
